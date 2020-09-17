a49954cbd9f8c0b0e89eda7fb1426126d961eea0
Gowtham <DEV@T2Sadmins-MacBook-Air.local>
2020-09-17 18:24:56 +0530

    test added

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/tools/i);
+  const linkElement = getByText(/username/i);
   expect(linkElement).toBeInTheDocument();
 });
